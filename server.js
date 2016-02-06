06a2e045063fcdd3d3a282bcf70a3065c41d1782
Chan Yik Hong <chanyikhong@Chans-MacBook-Pro.local>
2016-02-06 22:46:50 +0800

    Add POST for todos

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{
 	id: 1,
-	description: 'Meet mom for lunch',
+	description: 'Meet Anita for dinner',
 	completed: false
 }, {
 	id: 2,
-	description: 'Go to market',
+	description: 'Go to supermarket',
 	completed: false
 }, {
 	id: 3,
-	description: 'Buy the juice',
+	description: 'Buy Milk',
 	completed: true
 }];
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
+var todoNextId = 4;
 
-// GET /todos
-// GET /todos/2
+app.use(bodyParser.json());
 
 app.get('/todos', function (req, res) {
 	res.json(todos);
@@ -41,6 +43,20 @@ app.get('/todos/:id', function (req, res) {
 	else
 		res.status(404).send();
 
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	// add id field
+	body.id = todoNextId++;
+	// push body into array
+	todos.push(body);
+	console.log(todos);
+	// console.log('description ' + body.description);
+
+	res.json(body);
 });
 
 app.listen(PORT, function () {
