c1ad2886966e844efda144e394d8e2a8a556a7c2
Chan Yik Hong <chanyikhong@Chans-MacBook-Pro.local>
2016-02-07 15:55:55 +0800

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,42 +1,28 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet Anita for dinner',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to supermarket',
-	completed: false
-}, {
-	id: 3,
-	description: 'Buy Milk',
-	completed: true
-}];
+var todos = [];
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
-var todoNextId = 4;
+var todoNextId = 1;
 
 app.use(bodyParser.json());
 
+// GET /todos
 app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
-	// Iterate of todos array. Find the match.
-	// res.send(todos);
-	todos.forEach(function (todo) {
-		if (todo.id === todoID)
-			matchedTodo = todo;
-	});
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
 
 	if (matchedTodo)
 		res.json(matchedTodo);
@@ -47,14 +33,19 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description' , 'completed'); // User _.pick to only pick description and completed
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+		return res.status(400).send();
+	}
+
+	// set body.description to be trimmed value
+	body.description = body.description.trim();
 
 	// add id field
 	body.id = todoNextId++;
 	// push body into array
 	todos.push(body);
-	console.log(todos);
-	// console.log('description ' + body.description);
 
 	res.json(body);
 });
