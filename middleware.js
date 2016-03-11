9614c829068dadf908b89f10d8691a7cdfb0afb9
Chan Yik Hong <chanyikhong@Chans-MacBook-Pro.local>
2016-03-11 16:15:30 +0800

    Adding DELETE /users/login

--- middleware.js
+++ middleware.js
@@ -1,11 +1,25 @@
+var cryptojs = require('crypto-js');
 module.exports = function(db) {
 	return {
 		requireAuthentication: function(req, res, next) {
-			var token = req.get('Auth');
-			db.user.findByToken(token).then(function(user) {
+			var token = req.get('Auth') || '';
+
+			// Find whether the token is inside the database
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance) {
+				if (!tokenInstance) {
+					throw new Error();
+				}
+				// Find User by Token
+				req.token = tokenInstance;
+				return db.user.findByToken(token);
+			}).then(function(user) {
 				req.user = user;
 				next();
-			}, function() {
+			}).catch(function() {
 				res.status(401).send();
 			});
 		}
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {
 		userId: req.user.get('id')
-	}; 
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed == 'true') {
 		where.completed = true;
@@ -161,19 +161,31 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.stat(401).send();
-		}
-	}, function() {
+		userInstance = user;
+
+		return db.token.create({
+			token: token
+		});
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function() {
 		res.status(401).send();
 	});
 
 
+});
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
+	});
 });
 
 db.sequelize.sync({
